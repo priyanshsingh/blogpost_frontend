97ae191fa340a9dfbbcfcd8780ed133bb39bdffe
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-05-14 19:55:21 +0530

    updating blogs functionality enabled in backend

--- server/routes/home.js
+++ server/routes/home.js
@@ -5,7 +5,17 @@ const utils = require('../lib/utils')
 router.get('/blogs', utils.validateLogin, async (req, res, next)=>{
     User.find({}, {blogs: {_id: 1, title: 1, content: 1}, _id: 1})
     .then(data => {
-        return res.status(200).json({success: true, message: "blogs fetched successfully", blogs: data})
+        let blogs = []
+        for(i = 0; i<data.length; i++){
+            if(data[i].blogs.length !== 0){
+                // blogs.push(data[i].blogs);
+                for(j = 0; j<data[i].blogs.length; j++){
+                    blogs.push(data[i].blogs[j])
+                }
+            }
+        }
+
+        return res.status(200).json({success: true, message: "blogs fetched successfully", blogs: blogs})
     })
     .catch(err => {
         console.log(`error occured while fetching blogs, error message: ${err.message},\nerror:${err}`)
@@ -13,9 +23,21 @@ router.get('/blogs', utils.validateLogin, async (req, res, next)=>{
     })
 })
 
-router.patch('/addBlog', async (req, res, next)=>{
+/*
+router.get('/', (req, res, next)=>{
+    User.find({})
+    .then(data => {
+        res.status(200).json({success: false, data: data})
+    })
+    .catch(err => {
+        res.status(400).json({success: false})
+    })
+})
+*/
+
+router.patch('/addBlog', utils.validateLogin, async (req, res, next)=>{
     const blog = req.body.blog
-    const id = req.body.id
+    const id = req.jwt.sub
     await User.updateOne({ _id: id },
         {
             $push : {
